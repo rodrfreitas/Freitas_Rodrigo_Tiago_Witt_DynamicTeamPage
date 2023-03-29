4eeecfc45c6a92a32eaa32861b7c839420a80f2b
rodrfreitas <r_rochadefreitas@fanshaweonline.ca>
2023-03-29 16:39:30 -0400

    added proper lightbox bg img loading

--- js/team_data.js
+++ js/team_data.js
@@ -2,18 +2,18 @@ let team_data = {
     rod: {
         name: "Rodrigo Rocha de Freitas",
         bio: "Hi! I am Rodrigo Freitas, a UX/UI and Graphic Designer with experience in designing solutions and digital products for different industries. My skills include creating web and mobile design systems, developing interactive high-fidelity prototypes, working on information architecture, and interaction design. I always prioritize the person behind the data when solving problems for my users and stakeholders.",
-        img: "../images/rod_bg.png"
+        img: "images/rod_bg.png"
     },
 
     tiago: {
         name: "Tiago Witt",
         bio: "Hi There, Tiago Here! I am an experienced 3D Modeler, that brings whats is real to a digital world. As an experienced 3D modeler and motion graphics designer, I specialize in taking creative ideas and drawings and transforming them into the digital world. With my passion for the 3D field, I'm able to create immersive experiences that meet the needs and expectations of both clients and consumers. I'm skilled in a variety of programs, including Blender for 3D modeling and game asset preparation, Substance Painter for texturing and effects, and Cinema 4D for motion design and animation. Additionally, I use Adobe After Effects and Premiere, which allows me to prepare and present final video editions.",
-        img: "../images/tiago_bg.png"
+        img: "images/tiago_bg.png"
     },
 
     peter: {
         name: "Peter the cat",
         bio: "Meet Peter, the agile cat who doubles as a Scrum Master and a maker of allergies. Peter is a pro at facilitating agile meetings, guiding teams through sprints, and keeping everyone on track. But don't let his cute and cuddly appearance fool you - he's a tough cat when it comes to enforcing deadlines and ensuring that everyone follows the agile process.",
-        img: "../images/peter_bg.png"
+        img: "images/peter_bg.png"
     }
 }
